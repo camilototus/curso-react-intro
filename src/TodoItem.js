675bdbaac3db781b4bb7e9e5eb8fe98a3c462810
camilo rios <camilototus180@gmail.com>
2023-05-31 18:24:31 -0500

    se agregaron los archivos de TODOs counter, item, list, search

---
+++ src/TodoItem.js
@@ -0,0 +1,12 @@
+
+function TodoItem() {
+    return (
+      <li>
+        <span>V</span>
+        <p>Vamos con toda a Aprender FE</p>
+        <span>X</span>
+      </li>
+    );
+  }
+
+  export { TodoItem }
