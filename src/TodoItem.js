c0c0c80224b7698dd0890674ce78d7508996715a
camilo rios <camilototus180@gmail.com>
2023-06-02 17:57:15 -0500

    Agregamos y creamos archivos de iconos y modificamos sus estilos, y le volvimos a dar funcionalidad a los icono

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,15 +1,18 @@
+import { CompleteIcon } from './completeIcon'
+import { DeleteIcon } from './deleteIcon'
 import './TodoItem.css'
 
 function TodoItem(props) {
     return (
       <li className='TodoItem'>
-        <span className={`Icon Icon-check ${props.completed && "Icon-check--active"}`} 
-        onClick={props.onComplete}
-        >V</span>
+        <CompleteIcon 
+          completed={props.completed}
+          onComplete={props.onComplete}
+        />
         <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>{props.text}</p>
-        <span className={`Icon Icon-delete`}
-        onClick={props.onDelete}
-        >X</span>
+        <DeleteIcon 
+          onDelete={props.onDelete}
+        />
       </li>
     );
   }
